5c8a0b4df2a73f921fec22f78f95639d0defe01e
Beto Drumond <humbertowdrumond@gmail.com>
2020-10-11 10:10:01 -0300

    2.0v

--- approved_or_not.py
+++ approved_or_not.py
@@ -1,4 +1,5 @@
 def verification(grade):
+    # The function ensures that the entered value is a valid value
     while grade < 0 or grade > 10:
         grade = float(input('Invalid number, try again.\n'))
 
@@ -6,10 +7,13 @@ def verification(grade):
 
 
 def average(m1, m2, m3):
+    # The function calculates the arithmetic average of three grades
     return (m1 + m2 + m3) / 3
 
 
 def status():
+    # This function use the average to calculate and
+    # return the approval status.
     x = average(n1, n2, n3)
 
     print('-------------------------------------------------------')
@@ -30,6 +34,7 @@ def status():
 print('-------------------------------------------------------')
 print('Lets find out if the student was approved, or not')
 
+# It receive each grade and verify if its a valid number.
 n1 = float(input('Insert the first note (Between 0 and 10).\n'))
 if n1 < 0 or n1 > 10:
     n1 = float(verification(n1))
@@ -44,4 +49,6 @@ n3 = float(input('And, finally, insert the third note (Between 0 and 10).\n'))
 if n3 < 0 or n3 > 10:
     n3 = float(verification(n3))
 
+# It calculates the arithmetic average and
+# return it with the approval status.
 status()
